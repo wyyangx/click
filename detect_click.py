9a6dc3d65b15964b33362bd3432f06d740cb4bc3
wyyangx <eagleywy@126.com>
2018-07-13 18:33:31 +0800

    tkeo

--- detect_click.py
+++ detect_click.py
@@ -17,14 +17,6 @@ def mkdir(path):
         # 创建目录操作函数
         os.makedirs(path)
 
-
-def bit24_2_32(sub_bytes):
-    if sub_bytes[2] < 128:
-        return sub_bytes + b'\x00'
-    else:
-        return sub_bytes + b'\xff'
-
-
 if __name__ == '__main__':
 
     dict = {'0': '', '1': '', '2': ''}
@@ -40,49 +32,22 @@ if __name__ == '__main__':
         dst_path = "./Data/Click/%(class)s" % {'class': key}
         mkdir(dst_path)
         for pathname in wav_files:
-            wave_file = wave.open(pathname, 'rb')
-            params = wave_file.getparams()
-            channels, sampleWidth, frameRate, frames = params[:4]
-            data_bytes = wave_file.readframes(frames)  # 读取音频，字符串格式
-            wave_file.close()
 
-            int2float = 1
-            wave_data = np.zeros(channels*frames)
-            if sampleWidth == 2:
-                int2float = (2 ** 15) - 1
-                wave_data = np.fromstring(data_bytes, dtype=np.int16)  # 将字符串转化为int
-            elif sampleWidth == 3:
-                int2float = (2**23) - 1
-                '''
-                new_data_bytes = b''
-                for i in np.arange(channels*frames):
-                    sub_bytes = data_bytes[i*3:(i*3+3)]
-                    sub_bytes = bit24_2_32(sub_bytes)
-                    new_data_bytes += sub_bytes
-                wave_data = np.fromstring(new_data_bytes, dtype=np.int32)  # 将字符串转化为int
-                '''
-                samples = np.zeros(channels*frames)
-                for i in np.arange(samples.size):
-                    sub_bytes = data_bytes[i*3:(i*3+3)]
-                    sub_bytes = bit24_2_32(sub_bytes)
-                    samples[i] = struct.unpack('i', sub_bytes)[0]
-                wave_data = samples
-
-            '''                
-            for i in np.arange(wave_data.size):
-                wave_data[i] = wave_data[i]/int2float
-            '''
-
-            wave_data = np.reshape(wave_data, [frames, channels])
+            wave_data, frameRate = find_click.read_wav_file("Set1-A2-H17-081406-0000-0030-1225-1255loc.wav")
 
             fl = 5000
-            fwhm = 0.0004
+            fwhm = 0.0008
             fdr_threshold = 0.62
-            click_index = find_click.find_click_fdr_tkeo(wave_data, frameRate, fl, fwhm, fdr_threshold, 128)
+            click_index, xn = find_click.find_click_fdr_tkeo(wave_data, frameRate, fl, fwhm, fdr_threshold, 256, 8)
+
+            scale = 2 ** 15 / max(xn)
+            for i in np.arange(xn.size):
+                xn[i] = xn[i] * scale
 
             for j in range(click_index.shape[0]):
                 index = click_index[j]
-                click_data = wave_data[index[0]:index[1], 0]
+                # click_data = wave_data[index[0]:index[1], 0]
+                click_data = xn[index[0]:index[1]]
 
                 click_data = click_data.astype(np.short)
                 filename = "%(path)s/click_%(n)04d.wav" % {'path': dst_path, 'n': count}
--- find_click.py
+++ find_click.py
@@ -4,6 +4,37 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 import math
+import struct
+
+def bit24_2_32(sub_bytes):
+    if sub_bytes[2] < 128:
+        return sub_bytes + b'\x00'
+    else:
+        return sub_bytes + b'\xff'
+
+
+def read_wav_file(file_path):
+    wave_file = wave.open(file_path, 'rb')
+    params = wave_file.getparams()
+    channels, sampleWidth, frameRate, frames = params[:4]
+    data_bytes = wave_file.readframes(frames)  # 读取音频，字符串格式
+    wave_file.close()
+
+    wave_data = np.zeros(channels * frames)
+    if sampleWidth == 2:
+        wave_data = np.fromstring(data_bytes, dtype=np.int16)  # 将字符串转化为int
+    elif sampleWidth == 3:
+        samples = np.zeros(channels * frames)
+        for i in np.arange(samples.size):
+            sub_bytes = data_bytes[i * 3:(i * 3 + 3)]
+            sub_bytes = bit24_2_32(sub_bytes)
+            samples[i] = struct.unpack('i', sub_bytes)[0]
+        wave_data = samples
+
+    wave_data = wave_data.astype(np.float)
+    wave_data = np.reshape(wave_data, [frames, channels])
+
+    return wave_data, frameRate
 
 
 def list_wav_files(root_path):
@@ -21,25 +52,35 @@ def list_wav_files(root_path):
     return list
 
 
-def find_click_fdr_tkeo(xn, fs, fl, fwhm, fdr_threshold, ns):
+def find_click_fdr_tkeo(xn, fs, fl, fwhm, fdr_threshold, ns, snv_threshold = 10):
     if xn.ndim > 1:
         xn = xn[:, 0]
 
-    Wn = 2 * fl / fs  # Convert 3 - dB frequency
-    b, a = signal.butter(8, Wn, 'high')
+    Wn = fl / fs  # Convert 3 - dB frequency
+    b, a = signal.butter(5, Wn, 'highpass')
     xn = signal.filtfilt(b, a, xn)
+
+    back_ground_energy = np.sum(xn ** 2) / len(xn)
+
+    '''
+    plt.figure()
+    plt.plot(np.arange(0, len(xn)) * (1.0 / fs), xn)
+    plt.show()
+    '''
 
     length = len(xn)
     xn_0 = xn[1:(length - 1)]
     xn_1 = xn[0:(length - 2)]
     xnp1 = xn[2:length]
 
-    tken = xn_0 * xn_0 - xnp1 * xn_1
+    tkeo = xn_0 * xn_0 - xnp1 * xn_1
+
     '''
     plt.figure()
-    plt.plot(np.arange(0, len(tken)) * (1.0 / frameRate), tken)
+    plt.plot(np.arange(0, len(tkeo)) * (1.0 / fs), tkeo)
     plt.show()
     '''
+
 
     # rTK = fwhm / (4 * np.sqrt(np.log(2)))
     rG = fwhm / (2 * np.sqrt(2 * np.log(2)))
@@ -55,13 +96,13 @@ def find_click_fdr_tkeo(xn, fs, fl, fwhm, fdr_threshold, ns):
 
     '''
     plt.figure()
-    plt.plot(np.arange(0, len(hMAF1)) * (1.0 / frameRate), hMAF1)
-    plt.plot(np.arange(0, len(hMAF1)) * (1.0 / frameRate), hMAF2)
+    plt.plot(np.arange(0, len(hMAF1)) * (1.0 / fs), hMAF1)
+    plt.plot(np.arange(0, len(hMAF1)) * (1.0 / fs), hMAF2)
     plt.show()
     '''
 
-    hMAF1 = signal.convolve(tken, hMAF1)
-    hMAF2 = signal.convolve(tken, hMAF2)
+    hMAF1 = signal.convolve(tkeo, hMAF1)
+    hMAF2 = signal.convolve(tkeo, hMAF2)
 
     length = len(hMAF1)
     hMAF1 = hMAF1[N:(length - N)]
@@ -72,6 +113,13 @@ def find_click_fdr_tkeo(xn, fs, fl, fwhm, fdr_threshold, ns):
     for j in range(len(fdr)):
         if fdr[j] < -0.3:
             fdr[j] = -0.3
+
+    '''
+    plt.figure()
+    plt.plot(np.arange(0, len(hMAF1)), hMAF1)
+    plt.plot(np.arange(0, len(hMAF1)), hMAF2)
+    plt.show()
+    '''
 
     '''
     plt.figure()
@@ -100,6 +148,28 @@ def find_click_fdr_tkeo(xn, fs, fl, fwhm, fdr_threshold, ns):
                     max_fdr = fdr[max_idx]
                     max_idx = k
 
+            while beg_idx > 0:
+                if fdr[beg_idx-1] > 0.3:
+                    beg_idx -= 1
+                else:
+                    break
+
+            while end_idx < len(fdr):
+                if fdr[end_idx+1] > 0.3:
+                    end_idx += 1
+                else:
+                    break
+
+            sub_xn = xn[beg_idx:end_idx]
+            energy = np.sum(sub_xn ** 2) / len(sub_xn)
+
+            snv = 10 * np.log10(energy/back_ground_energy)
+
+            if snv < snv_threshold:
+                beg_idx = -1
+                end_idx = -1
+                continue
+
             beg_idx = int(max_idx - ns/2)
             end_idx = int(max_idx + ns/2)
             if beg_idx >= 0 and end_idx <= len(fdr):
@@ -108,35 +178,33 @@ def find_click_fdr_tkeo(xn, fs, fl, fwhm, fdr_threshold, ns):
             beg_idx = -1
             end_idx = -1
 
-    return np.array(click_index)
+    return np.array(click_index), xn
 
 
 if __name__ == '__main__':
-    wave_file = wave.open("Click01.wav", 'rb')
-    params = wave_file.getparams()
-    channels, sampleWidth, frameRate, frames = params[:4]
-    str_data = wave_file.readframes(frames)                    # 读取音频，字符串格式
-    wave_file.close()
-    wave_data = np.fromstring(str_data, dtype=np.int16)         # 将字符串转化为int
-    wave_data = np.reshape(wave_data, [frames, channels])
+    wave_data, frameRate = read_wav_file("Click01.wav")
 
     fl = 5000
-    fwhm = 0.0004
+    fwhm = 0.0008
     fdr_threshold = 0.6
-    click_index = find_click_fdr_tkeo(wave_data, frameRate, fl, fwhm, fdr_threshold, 128)
+    click_index, xn = find_click_fdr_tkeo(wave_data, frameRate, fl, fwhm, fdr_threshold, 256, 8)
+
+    scale = 2 ** 15 / max(xn)
+    for i in np.arange(xn.size):
+        xn[i] = xn[i] * scale
 
     for j in range(click_index.shape[0]):
         index = click_index[j]
-        click_data = wave_data[index[0]:index[1], 0]
+        # click_data = wave_data[index[0]:index[1], 0]
+        click_data = xn[index[0]:index[1]]
 
         click_data = click_data.astype(np.short)
         filename = "click_%(n)04d.wav" % {'n': j}
         f = wave.open(filename, "wb")
         # set wav params
         f.setnchannels(1)
-        f.setsampwidth(sampleWidth)
+        f.setsampwidth(2)
         f.setframerate(frameRate)
-        # turn the data to string
         f.writeframes(click_data.tostring())
         f.close()
 
